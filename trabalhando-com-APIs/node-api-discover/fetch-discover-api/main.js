cf0071ad6c38992e3e1de5ecf4dcdd54ec387eff
luistomasini <luis_tomasini20@hotmail.com>
2023-06-05 21:48:21 -0300

    Continuação de estudos sobre Fetch no Front-end

---
+++ trabalhando-com-APIs/node-api-discover/fetch-discover-api/main.js
@@ -0,0 +1,22 @@
+const url = "http://localhost:5500/api"
+
+function getUsers() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => renderApiResult.textContent = JSON.stringify(data))
+    .catch(error => console.error(error))
+}
+
+function getUser() {
+    fetch(`${url}/1`)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        userCity.textContent = data.city
+        userAvatar.src = data.avatar
+    })
+    .catch(error => console.error(error))
+}
+
+getUser()
+getUser()
