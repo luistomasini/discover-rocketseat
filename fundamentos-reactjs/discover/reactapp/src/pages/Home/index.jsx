a7bcaca39d3bb63b63dfd2992cb6d54a4e2ff7bd
luistomasini <luis_tomasini20@hotmail.com>
2023-06-07 23:05:32 -0300

    continuação de estudos - estilizando o css

--- fundamentos-reactjs/discover/reactapp/src/pages/Home/index.jsx
+++ fundamentos-reactjs/discover/reactapp/src/pages/Home/index.jsx
@@ -2,10 +2,10 @@ import './style.css';
 
 export function Home() {
   return (
-    <>
+    <div className="container">
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..."/>
       <button type="button">Adicionar</button>
-    </>    
+    </div>    
   )
 }
